ee64ac23647d6bf819f657a5443c0cdb0487e04e
Miikka Saxlund <miikka.saxlund@outlook.com>
2020-01-12 21:24:25 +0200

    Updated build file

--- build.bat
+++ build.bat
@@ -1,6 +1,10 @@
 @echo off
-gcc -o ./dist/calc ./src/main.c
-echo calc.exe built
+gcc -o ./dist/calc ./src/calc.c
+echo calc.exe built succesfully
 gcc -o ./dist/rel ./src/rel.c
-echo rel.exe built
+echo rel.exe built succesfully
+gcc -o ./dist/rc ./src/rc.c
+echo rc.exe built succesfully
+gcc -o ./dist/rcb ./src/rcb.c
+echo rcb.exe built succesfully
 echo All files built succesfully
