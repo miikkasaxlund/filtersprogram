aa2f9a2f208706682335f7f61dc203ee25115a1e
Miikka Saxlund <miikka.saxlund@outlook.com>
2020-01-12 21:25:56 +0200

    Renamed main.c to calc.c

--- src/calc.c
+++ src/calc.c
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
- * File:   main.c
- * Author: miikk
+ * File:   calc.c
+ * Author: Miikka Saxlund
  *
  * Created on 03 January 2020, 14:50
  */
