b8923d968a2ae15a222d0a54ed93f8379173321f
Miikka Saxlund <miikka.saxlund@outlook.com>
2020-01-14 10:19:21 +0200

    Added more sources to build script

--- build.bat
+++ build.bat
@@ -9,4 +9,8 @@ gcc -o ./dist/rcb ./src/rcb.c
 echo rcb.exe built succesfully
 gcc -o ./dist/rws ./src/rws.c
 echo rws.exe built succesfully
+gcc -o ./dist/rtws ./src/rtws.c
+echo rws.exe built succesfully
+gcc -o ./dist/rcws ./src/rcws.c
+echo rcws.exe built succesfully
 echo All files built succesfully
