0e0b3da4dd41403363fc9d9f48a937e6c913ef77
Miikka Saxlund <miikka.saxlund@outlook.com>
2020-01-14 22:19:19 +0200

    Added comments

--- src/rc.c
+++ src/rc.c
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
- * File:   main.c
- * Author: miikk
+ * File:   rc.c
+ * Author: Miikka Saxlund
+ * Brief: A filter program to remove one-line comments from the input
  *
  * Created on 03 January 2020, 14:50
  */
@@ -17,55 +12,82 @@
 #include <ctype.h>
 
 /*
- * A filter program to remove comments from the input
+ * @brief Filter function
+ * 
+ * Filters one-line comments from the stream
+ * 
+ * @param input a pointer to the standard input stream
+ * @param output a pointer to the standard output stream
  */
 
 int filter( FILE *input, FILE *output )
 {
-    int emptylines = 0;
-    char buffer[5000], *fp;
+    /* 
+     * Define the buffer (max-length 5000 characters), 
+     * buffer pointer and character variables
+     */
+    char buffer[5000], *fp, ch, prevch;
 
-    fseek(input, 0L, SEEK_END); 
-    const int size = ftell(input); 
-    char* retcontent;
-    retcontent = (char *)malloc(size * sizeof(int));
-    char ch;
-
-    fpos_t position;
-
+    // Loop through the stream until the end-of-file indicator
     while ( !feof( input ) ) {
+        // Return in case of occurring error indicator
         if ( ferror( input ) ) {
             return 1;
         }
-        // Do stuff here:
+        /* 
+         * Get a character from the stream and advance
+         * the position indicator for the stream. Save
+         * the found character to the prevchar variable
+         * for later usage
+         */
         ch = fgetc(input);
-        fgetpos(input, &position);
-        // Check for a slash
+        prevch = ch;
+        // Check for consecutive slash characters
         if (ch == '/') {
-          // Check the next character
-          ch = fgetc(input);
-          // If slash again
-          if (ch == '/') {
-            // Advance in the stream until a new line
-            while ((ch = fgetc(input)) != '\n') ;
-          } else {
-            /*
-             Return the slash to the stream if not a comment.
-             Probably should be done using fseek to go backward
-             in the stream, but could not make it work.
-            */
-            fputc('/', output);
-          }
+            ch = fgetc(input);
+            // If consecutive slash character is found;
+            if (ch == '/') {
+              /*
+               * Advance in the stream (at least once) 
+               * until a new-line character is found
+               */
+              do {
+                ch = fgetc(input);
+              } while (ch != '\n');
+            }
+            /* 
+             * If only one slash character was found, write
+             * the previously found character to the stream
+             */
+            else {
+              fputc(prevch, output);
+            }
         }
-        fputc(ch, output);
+        /*
+         * Check that the end of-file-indicator is not found
+         * and write the character to the output stream
+         */
+        if (!feof( input )){
+          fputc(ch, output);
+        }
     }
     return 0;
 }
 
+/*
+ * @brief Main function
+ * 
+ * Passes the stdin stream to the filter function to be filtered
+ * and flushes the output buffer so it can be outputted to the
+ * stdout output stream
+ */
+
 int main(void)
 {
+    // Get the return value from the filter function
     const int retval = filter( stdin, stdout );
     // Flush the output buffer and move the buffered data to console
     fflush( stdout );
+    // Return the return value
     return retval;
 }
