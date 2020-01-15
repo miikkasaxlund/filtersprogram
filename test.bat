ba4831aaac6649567fa4e1559ae791c863ad4f41
Miikka Saxlund <miikka.saxlund@outlook.com>
2020-01-15 12:41:15 +0200

    Updated the test.bat

--- test.bat
+++ test.bat
@@ -1,6 +1,2 @@
 @echo off
-echo Without removing the whitespace
-cat testfile.txt | .\dist\calc.exe
-echo.
-echo After removing the whitespace
-cat testfile.txt | .\dist\rtws.exe | .\dist\calc.exe
+cat testfile.txt | .\dist\rws.exe
